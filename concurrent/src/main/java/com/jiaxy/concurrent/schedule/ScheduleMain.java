39000e4f6189e8de5efd6ce01b9ca5003f5a7e8a
wutao <wutao@jd.com>
2015-05-25 14:54:54 +0800

    ScheduledExecutorService

--- concurrent/src/main/java/com/jiaxy/concurrent/schedule/ScheduleMain.java
+++ concurrent/src/main/java/com/jiaxy/concurrent/schedule/ScheduleMain.java
@@ -17,13 +17,36 @@ public class ScheduleMain {
 
     public static void main(String[] args){
 
-        ScheduledExecutorService sheduler = Executors.newSingleThreadScheduledExecutor();
+        /*ScheduledExecutorService sheduler = Executors.newSingleThreadScheduledExecutor();
         sheduler.scheduleWithFixedDelay(new Runnable() {
             @Override
             public void run() {
                 System.out.println(new Date());
             }
-        },2,2, TimeUnit.SECONDS);
+        },2,2, TimeUnit.SECONDS);*/
 
+        scheduleFixRate();
+
+    }
+
+
+    /**
+     * scheduleAtFixedRate 如果方法的执行时间超过period ，方法执行完成后，立即执行下次调用
+     *
+     */
+    public static void scheduleFixRate(){
+        ScheduledExecutorService sheduler = Executors.newSingleThreadScheduledExecutor();
+        sheduler.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(new Date());
+                try {
+                    Thread.sleep(5000);
+                    System.out.println("after---"+new Date());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },1,1,TimeUnit.SECONDS);
     }
 }
